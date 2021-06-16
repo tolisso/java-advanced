f2f0a24c16810d74a84e2e4aebd2959b3899055a
tolisso <tolisso@e.malko@bk.ru>
2021-06-16 04:38:16 +0300

    old structure removed

--- info/kgeorgiy/ja/malko/walk/RecursiveWalk.java
+++
@@ -1,9 +0,0 @@
-package info.kgeorgiy.ja.malko.walk;
-
-public class RecursiveWalk {
-
-    public static void main(final String[] args) {
-        // :NOTE: Дублирование
-        Walk.run(args, Integer.MAX_VALUE);
-    }
-}
